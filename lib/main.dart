ceb308913a088eb3287e5924ab41a97ad6c8cd01
fvieir <fcvieira2@gmail.com>
2022-09-29 14:27:14 -0300

    Adicionando models Transação da lista

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:expenses/models/trasanctions.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const Expenses());
@@ -18,6 +19,21 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final List<Transaction> _transactions = [
+      Transaction(
+        id: 't1',
+        title: 'Novo tênis Adidas',
+        value: 370.76,
+        date: DateTime.now(),
+      ),
+      Transaction(
+        id: 't2',
+        title: 'Conta de água',
+        value: 70.35,
+        date: DateTime.now(),
+      ),
+    ];
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.purple,
@@ -26,17 +42,21 @@ class MyHomePage extends StatelessWidget {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: const [
-          SizedBox(
+        children: [
+          const SizedBox(
             child: Card(
               color: Colors.purple,
               elevation: 5,
               child: Text('Gráfico'),
             ),
           ),
-          Card(
-            child: Text('Lista de transações'),
-          )
+          Column(
+            children: _transactions.map((tr) {
+              return Card(
+                child: Text(tr.title),
+              );
+            }).toList(),
+          ),
         ],
       ),
     );
