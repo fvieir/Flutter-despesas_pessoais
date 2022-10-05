27ba4ea8c811bb90be722a716a0fa9f0a2907f68
fvieir <fcvieira2@gmail.com>
2022-10-05 17:31:20 -0300

    Ajustando altura do teclado

--- lib/components/transaction_form.dart
+++ lib/components/transaction_form.dart
@@ -43,67 +43,74 @@ class _TransactionFormState extends State<TransactionForm> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 5,
-      child: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Column(
-          children: [
-            TextField(
-              controller: textController,
-              onSubmitted: (_) => _submitForm(),
-              decoration: const InputDecoration(
-                labelText: 'Título',
+    return SingleChildScrollView(
+      child: Card(
+        elevation: 5,
+        child: Padding(
+          padding: EdgeInsets.only(
+            top: 10,
+            right: 10,
+            left: 10,
+            bottom: 10 + MediaQuery.of(context).viewInsets.bottom,
+          ),
+          child: Column(
+            children: [
+              TextField(
+                controller: textController,
+                onSubmitted: (_) => _submitForm(),
+                decoration: const InputDecoration(
+                  labelText: 'Título',
+                ),
               ),
-            ),
-            TextField(
-              controller: valueController,
-              onSubmitted: (_) => _submitForm(),
-              keyboardType: const TextInputType.numberWithOptions(
-                decimal: true,
+              TextField(
+                controller: valueController,
+                onSubmitted: (_) => _submitForm(),
+                keyboardType: const TextInputType.numberWithOptions(
+                  decimal: true,
+                ),
+                decoration: const InputDecoration(
+                  labelText: 'Valor R\$:',
+                ),
               ),
-              decoration: const InputDecoration(
-                labelText: 'Valor R\$:',
-              ),
-            ),
-            SizedBox(
-              height: 70,
-              child: Row(
-                children: [
-                  Expanded(
-                    child: Text(
-                      _selectDate == null
-                          ? 'Nenhuma data Selecionada.'
-                          : 'Data selecionada: ${DateFormat('dd/MM/y').format(_selectDate!)}',
-                    ),
-                  ),
-                  TextButton(
-                    onPressed: _showDatePicker,
-                    child: const Text(
-                      'Selecione uma data',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
+              SizedBox(
+                height: 70,
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Text(
+                        _selectDate == null
+                            ? 'Nenhuma data Selecionada.'
+                            : 'Data selecionada: ${DateFormat('dd/MM/y').format(_selectDate!)}',
                       ),
                     ),
-                  )
-                ],
+                    TextButton(
+                      onPressed: _showDatePicker,
+                      child: const Text(
+                        'Selecione uma data',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
               ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: <Widget>[
-                ElevatedButton(
-                  onPressed: _submitForm,
-                  child: Text(
-                    'Nova Transação',
-                    style: TextStyle(
-                      color: Theme.of(context).textTheme.labelLarge?.color,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  ElevatedButton(
+                    onPressed: _submitForm,
+                    child: Text(
+                      'Nova Transação',
+                      style: TextStyle(
+                        color: Theme.of(context).textTheme.labelLarge?.color,
+                      ),
                     ),
                   ),
-                ),
-              ],
-            ),
-          ],
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
